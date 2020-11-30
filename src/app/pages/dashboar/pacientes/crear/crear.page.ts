f59c4ba006ae8080d65e3cc2a80645d03ab1e6dc
Alan <pasantes2@berrywhale.com>
2020-11-30 01:27:51 -0400

    add

--- src/app/pages/dashboar/pacientes/crear/crear.page.ts
+++ src/app/pages/dashboar/pacientes/crear/crear.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PacienteRequestService } from 'src/app/services/pacientes/paciente-request.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear.page.scss'],
 })
 export class CrearPage implements OnInit {
-
-  constructor() { }
+  public form: FormGroup;
+  constructor(
+    private route: Router,
+    private pacienterequest: PacienteRequestService,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.initform();
+  }
+  initform(){
+    this.form  = this.fb.group({
+      cedula: ['',Validators.required],
+      tipeBlood: ['',Validators.required],
+      alerg: ['',Validators.required],
+      email: ['',]
+    })
   }
 
+  cargar(){
+    console.log(this.form.controls)
+    const body={
+      "cedula": this.form.controls.cedula.value,
+      "foto": "",
+      "email":"rm12345@gmail.com",
+      "bloodType": this.form.controls.tipeBlood.value,
+      "alergies": this.form.controls.alerg.value
+    }
+    this.pacienterequest.regPaciente(body).subscribe((res)=>{
+      console.log(res);
+    })
+  }
 }
--- src/app/services/pacientes/paciente-request.service.ts
+++ src/app/services/pacientes/paciente-request.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { apiUrl } from '../enviroment/enviroment.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PacienteRequestService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private auth: AuthService) { }
 
   getListPacientes(token,ref){
     console.log(`${apiUrl}${ref}?token=${token}`);
     return this.http.get(`${apiUrl}${ref}?token=${token}`);
+  }
+  regPaciente(body){
+    console.log(`${apiUrl}patient/register?token=${this.auth.getToken()}`)
+    return this.http.post(`${apiUrl}patient/register?token=${this.auth.getToken()}`,body);
   }
 }
