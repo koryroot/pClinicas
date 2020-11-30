25dfa7c205c44d0deb577648d08881fdf7538bb5
Alan <pasantes2@berrywhale.com>
2020-11-29 22:31:25 -0400

    .......

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FormAuthComponent } from './form-auth/form-auth.component';
 import { TableListComponent } from './table-list/table-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
 
 
 
 const components = [
-  FormAuthComponent,TableListComponent,NavbarComponent
+  FormAuthComponent,TableListComponent,NavbarComponent,SearchComponent
 ]
 
 @NgModule({
